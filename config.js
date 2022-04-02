2f57e8e149b61122055cb361f0adbb9f576332dc
Duckei1102 <95559858+Duckei1102@users.noreply.github.com>
2022-04-03 02:01:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,8 @@
 const nameGirl = 'Ạ<3';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'https://duckei1102.github.io/14-3cuaqhuong/';
 const eventName = 'Em Nhấp Vào Đây Đi ';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Gửi em cái này dù nó chẳng đáng là bao nhưng nó thay lời xin lỗi của anh đến em.Anh biết rằng mình đã làm điều gì đó sai trái nhưng nếu anh không xin lỗi em thì anh càng sai hơn nữa.khi xem xong chúc em có một giấc ngủ thật ngon để gạt đi những căng thẳng của một ngày chạy dl mệt mỏi.Anh yêu Em';
-
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
 const giftImage = 'QH1.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
