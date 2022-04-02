e2917fcc4b5bf29351c1d7f020cb07ee5daeb5b1
Duckei1102 <95559858+Duckei1102@users.noreply.github.com>
2022-04-03 02:02:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const giftUrl = 'https://duckei1102.github.io/14-3cuaqhuong/';
 const eventName = 'Em Nhấp Vào Đây Đi ';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Gửi em cái này dù nó chẳng đáng là bao nhưng nó thay lời xin lỗi của anh đến em.Anh biết rằng mình đã làm điều gì đó sai trái nhưng nếu anh không xin lỗi em thì anh càng sai hơn nữa.khi xem xong chúc em có một giấc ngủ thật ngon để gạt đi những căng thẳng của một ngày chạy dl mệt mỏi.Anh yêu Em';
-const giftImage = 'QH1.jpg';
+const giftImage = 'QH.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
